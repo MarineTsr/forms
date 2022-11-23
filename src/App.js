6817609f665bf3ef7e21ff05758a4a28581d4cd6
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-11-23 15:06:03 +0100

    Basic form and tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import { useForm } from "react-hook-form";
+
 function App() {
-  return <h1>Hello world !</h1>;
+  const { register, handleSubmit, getValues, watch } = useForm();
+
+  watch();
+
+  const formSubmit = (values) => {
+    console.log(values);
+  };
+
+  console.log(getValues());
+
+  return (
+    <div className="container my-5 px-5">
+      <h1 className="title is-4">Gestion des formulaires</h1>
+      <hr />
+      <form className="columns" onSubmit={handleSubmit(formSubmit)}>
+        <div className="column is-6 is-offset-3">
+          <div className="field">
+            <label htmlFor="lastname" className="label">
+              Nom
+            </label>
+            <div className="control">
+              <input
+                className="input"
+                id="lastname"
+                type="text"
+                {...register("lastname")}
+              />
+            </div>
+          </div>
+
+          <div className="field is-grouped is-grouped-centered">
+            <div className="control">
+              <button type="submit" className="button is-link">
+                Envoyer
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default App;
