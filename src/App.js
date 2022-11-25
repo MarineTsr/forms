997e3b997b129cf60403148a8585764b61872837
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-11-25 15:34:12 +0100

    Form validation with yup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function App() {
+  const userSchema = yup.object({
+    lastname: yup
+      .string()
+      .required("Le champ est obligatoire")
+      .min(1, "Le champ doit faire au moins 1 caractère")
+      .test("random", "C'est un non !", async () => {
+        try {
+          const response = await fetch("https://yesno.wtf/api");
+
+          if (response.ok) {
+            const result = await response.json();
+            return result.answer === "yes";
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }),
+    firstname: yup.string().min(1, "Le champ doit faire au moins 1 caractère"),
+    password: yup
+      .string()
+      .required("Le champ est obligatoire")
+      .min(8, "Le champ doit faire au moins 8 caractères"),
+    passwordConfirm: yup
+      .string()
+      .required("Le champ est obligatoire")
+      .oneOf(
+        [yup.ref("password"), ""],
+        "Les mots de passes ne sont pas identiques"
+      ),
+  });
+
   const {
     register,
     handleSubmit,
@@ -9,7 +42,11 @@ function App() {
     defaultValues: {
       firstname: "",
       lastname: "",
+      password: "",
+      passwordConfirm: "",
     },
+    mode: "onSubmit",
+    resolver: yupResolver(userSchema),
   });
 
   const formSubmit = (data, event) => {
@@ -34,16 +71,7 @@ function App() {
                 className={`input${errors?.lastname ? " is-danger" : ""}`}
                 id="lastname"
                 type="text"
-                {...register("lastname", {
-                  minLength: {
-                    value: 2,
-                    message: "Le nom est trop court",
-                  },
-                  required: {
-                    value: true,
-                    message: "Ce champ est requis",
-                  },
-                })}
+                {...register("lastname")}
               />
             </div>
             {errors?.lastname && (
@@ -62,16 +90,51 @@ function App() {
                 className={`input${errors?.firstname ? " is-danger" : ""}`}
                 id="firstname"
                 type="text"
-                {...register("firstname", {
-                  minLength: {
-                    value: 2,
-                    message: "Le prénom est trop court",
-                  },
-                })}
+                {...register("firstname")}
               />
             </div>
             {errors?.firstname && (
               <p className="help is-danger">{errors.firstname.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="column is-6">
+          <div className="field">
+            <label htmlFor="password" className="label">
+              Mot de passe
+            </label>
+            <div className="control">
+              <input
+                className={`input${errors?.password ? " is-danger" : ""}`}
+                id="password"
+                type="password"
+                {...register("password")}
+              />
+            </div>
+            {errors?.password && (
+              <p className="help is-danger">{errors.password.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="column is-6">
+          <div className="field">
+            <label htmlFor="passwordConfirm" className="label">
+              Confirmation du mot de passe
+            </label>
+            <div className="control">
+              <input
+                className={`input${
+                  errors?.passwordConfirm ? " is-danger" : ""
+                }`}
+                id="passwordConfirm"
+                type="password"
+                {...register("passwordConfirm")}
+              />
+            </div>
+            {errors?.passwordConfirm && (
+              <p className="help is-danger">{errors.passwordConfirm.message}</p>
             )}
           </div>
         </div>
