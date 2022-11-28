2f5589f5c4455f9bf54244cb6ed47f5d63909f24
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-11-28 17:26:49 +0100

    Form specific fields,  submission and errors management

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { yupResolver } from "@hookform/resolvers/yup";
 
 function App() {
   const userSchema = yup.object({
+    title: yup.string().nullable().required("Le champ est obligatoire"),
+    type: yup.string().required("Le champ est obligatoire"),
     lastname: yup
       .string()
       .required("Le champ est obligatoire")
@@ -32,25 +34,60 @@ function App() {
         [yup.ref("password"), ""],
         "Les mots de passes ne sont pas identiques"
       ),
+    rgpd: yup
+      .bool()
+      .required("Le champ est obligatoire")
+      .oneOf([true], "Vous devez approuver pour continuer"),
   });
+
+  const defaultValues = {
+    title: null,
+    type: "",
+    firstname: "",
+    lastname: "",
+    password: "",
+    passwordConfirm: "",
+    rgpd: false,
+  };
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    reset,
+    // setError,
+    clearErrors,
+    formState: { errors, isSubmitting },
   } = useForm({
-    defaultValues: {
-      firstname: "",
-      lastname: "",
-      password: "",
-      passwordConfirm: "",
-    },
+    defaultValues,
     mode: "onSubmit",
     resolver: yupResolver(userSchema),
   });
 
-  const formSubmit = (data, event) => {
-    console.log(data, event);
+  const formSubmit = async (data, event) => {
+    try {
+      clearErrors(); // Must clear errors because they won't be deleted automatically if they are not linked to a specific field (ex: global errors)
+
+      const response = await fetch("https://restapi.fr/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const results = await response.json();
+        console.log(results);
+        reset(defaultValues);
+        // throw new Error("Ceci est une erreur de test");
+      }
+    } catch (err) {
+      console.error(err);
+      // setError("globalErr", {
+      //   type: "globalErr",
+      //   message: err.message,
+      // });
+    }
   };
 
   return (
@@ -61,6 +98,65 @@ function App() {
         className="columns is-multiline"
         onSubmit={handleSubmit(formSubmit)}
       >
+        {errors?.globalErr && (
+          <div className="column is-12">
+            <article className="message is-danger">
+              <div className="message-body">{errors.globalErr.message}</div>
+            </article>
+          </div>
+        )}
+
+        <div className="column is-6">
+          <div className="field">
+            <label className="label">Civilité</label>
+            <div className="control">
+              <label className="radio mr-4">
+                <input
+                  type="radio"
+                  name="title"
+                  value="m"
+                  className="mr-1"
+                  {...register("title")}
+                />
+                M.
+              </label>
+              <label className="radio">
+                <input
+                  type="radio"
+                  name="title"
+                  value="mme"
+                  className="mr-1"
+                  {...register("title")}
+                />
+                Mme.
+              </label>
+            </div>
+            {errors?.title && (
+              <p className="help is-danger">{errors.title.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="column is-6">
+          <div className="field">
+            <label htmlFor="type" className="label">
+              Type de compte
+            </label>
+            <div className={`select${errors?.type ? " is-danger" : ""}`}>
+              <select id="type" {...register("type")}>
+                <option value="" disabled>
+                  Choisissez un type de compte
+                </option>
+                <option value="pro">Professionnel</option>
+                <option value="part">Particulier</option>
+              </select>
+            </div>
+            {errors?.type && (
+              <p className="help is-danger">{errors.type.message}</p>
+            )}
+          </div>
+        </div>
+
         <div className="column is-6">
           <div className="field">
             <label htmlFor="lastname" className="label">
@@ -140,9 +236,30 @@ function App() {
         </div>
 
         <div className="column is-12">
+          <div className="field">
+            <label className="checkbox">
+              <input
+                type="checkbox"
+                name="rgpd"
+                className="mr-1"
+                {...register("rgpd")}
+              />
+              J'accepte les conditions d'utilisation de ce service
+            </label>
+            {errors?.rgpd && (
+              <p className="help is-danger">{errors.rgpd.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className="column is-12">
           <div className="field is-grouped is-grouped-centered">
             <div className="control">
-              <button type="submit" className="button is-link">
+              <button
+                type="submit"
+                className="button is-link"
+                disabled={isSubmitting}
+              >
                 Envoyer
               </button>
             </div>
